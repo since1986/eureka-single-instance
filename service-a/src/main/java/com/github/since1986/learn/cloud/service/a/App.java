5bcf599339850d0526bca0086b39be96ae90359f
since1986 <385741668@qq.com>
2018-03-29 14:58:03 +0800

    增加了 EurekaClient 调用例子

--- service-a/src/main/java/com/github/since1986/learn/cloud/service/a/App.java
+++ service-a/src/main/java/com/github/since1986/learn/cloud/service/a/App.java
@@ -1,12 +1,31 @@
 package com.github.since1986.learn.cloud.service.a;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class App {
 
+    private final EurekaClient eurekaClient;
+
+    @Autowired
+    public App(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Scheduled(initialDelay = 1000 * 60 * 3, fixedRate = 1000 * 30)
+    public void printInstanceInfo() {
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("com-github-since1986-learn-cloud-service-b", false);
+        System.out.println(String.format("发现了 服务名为 %s IP为 %s 的服务.", instanceInfo.getAppName(), instanceInfo.getIPAddr()));
     }
 }
